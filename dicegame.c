38b75fd2bb02738a5eb86e0a2d87cba049752250
n <40595197+oceans73@users.noreply.github.com>
2019-07-11 23:59:33 +0900

    add final

--- dicegame.c
+++ dicegame.c
@@ -8,16 +8,18 @@ int main(void){
   scanf("%s",s);
   printf("Hello, %s!\n",s);
 
-  int num1,num2,total=0;
+  int num1,num2,num3,total=0;
 
   srand(time(NULL));
   num1 = rand() % 6 + 1;
   num2 = rand() % 6 + 1;
+  num3 = rand() % 6 + 1;
   total=num1+num2;
 
   printf("Rolling the dice...\n\
     Die 1: %d\n\
     Die 2: %d\n\
+    Die 3: %d\n\
     Total value: %d \n", num1,num2,total);
 
   if(total>7) printf("%s win!\n",s);
